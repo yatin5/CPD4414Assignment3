42ee562f2182cb49e6fd65b848e53f92f1e96d71
HP <HP@HP-PC.gateway.2wire.net>
2015-06-08 17:33:10 -0400

    implemented some override method with doPost and doPut

---
+++ src/java/databaseCredentials/productServlets.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseCredentials;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Set;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author HP
+ */
+@WebServlet("/products")
+public class productServlets extends HttpServlet {
+    
+    @Override
+     protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setHeader("Content-Type", "text/plain-text");
+        try {
+            PrintWriter output = response.getWriter();
+            String query = "SELECT * FROM product;";
+            if (!request.getParameterNames().hasMoreElements()) {
+                output.println(resultMethod(query));
+            } else {
+                int id = Integer.parseInt(request.getParameter("ProductID"));
+                output.println(resultMethod("SELECT * FROM product WHERE ProductID= ?", String.valueOf(id)));
+            }
+
+        } catch (IOException ex) {
+            System.err.println("Input output Exception: " + ex.getMessage());
+        }
+    }
+    
+
+ @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Set<String> keyValues = request.getParameterMap().keySet();
+
+        try {
+            PrintWriter output = response.getWriter();
+            if (keyValues.contains("ProductID") && keyValues.contains("name") && keyValues.contains("description")
+                    && keyValues.contains("quantity")) {
+                String ProductID = request.getParameter("ProductID");
+                String name = request.getParameter("name");
+                String description = request.getParameter("description");
+                String quantity = request.getParameter("quantity");
+                doUpdate("INSERT INTO product (ProductID,name,description,quantity) VALUES (?, ?, ?, ?)", ProductID, name, description, quantity);
+
+            } else {
+                response.setStatus(500);
+                output.println("Error: Not data found for this input. Please use a URL of the form /servlet?name=XYZ&age=XYZ");
+            }
+
+        } catch (IOException ex) {
+            System.err.println("Input Output Issue in doPost Method: " + ex.getMessage());
+        }
+
+    }
+     @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) {
+
+        Set<String> keySet = request.getParameterMap().keySet();
+        try (PrintWriter out = response.getWriter()) {
+            if (keySet.contains("ProductID") && keySet.contains("name") && keySet.contains("description") && keySet.contains("quantity")) {
+                String ProductID = request.getParameter("ProductID");
+                String name = request.getParameter("name");
+                String description = request.getParameter("description");
+                String quantity = request.getParameter("quantity");
+                doUpdate("update product set ProductID = ?, name = ?, description = ?, quantity = ? where ProductID = ?", ProductID, name, description, quantity, ProductID);
+            } else {
+                out.println("Error: Not data found for this input. Please use a URL of the form /products?id=xx&name=XXX&description=XXX&quantity=xx");
+            }
+        } catch (IOException ex) {
+            response.setStatus(500);
+            System.out.println("Error in writing output: " + ex.getMessage());
+        }
+    }
+    
+}
